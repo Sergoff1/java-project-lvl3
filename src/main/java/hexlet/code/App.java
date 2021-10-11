a4669afb46392dd149191487b41f60f4b2327484
Sergey Afonasyev <Afonasiew.serzh@yandex.ru>
2021-10-11 21:23:40 +0300

    CI check

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -3,6 +3,10 @@ package hexlet.code;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(hello());
+    }
+
+    public static String hello() {
+        return "Hello World!";
     }
 }
