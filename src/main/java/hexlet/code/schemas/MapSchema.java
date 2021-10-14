fce50ce994011d15581ce3f3ad21977aa57a0c9b
Sergey Afonasyev <Afonasiew.serzh@yandex.ru>
2021-10-14 21:03:26 +0300

    Add shape() and remove App

--- src/main/java/hexlet/code/schemas/MapSchema.java
+++ src/main/java/hexlet/code/schemas/MapSchema.java
@@ -5,23 +5,35 @@ import java.util.Map;
 public final class MapSchema extends BaseSchema {
 
     private Integer mapSize;
+    private Map<String, BaseSchema> shape;
 
     @Override
     public boolean isValid(Object o) {
-        if (isRequired && !(o instanceof Map)) {
+        if (isRequired() && !(o instanceof Map)) {
             return false;
         }
 
-        Map<Object, Object> map = (Map) o;
+        Map<Object, Object> map = (Map<Object, Object>) o;
 
         if (mapSize != null && !(map.size() == mapSize)) {
             return false;
         }
 
+        if (shape != null) {
+            for (String key : shape.keySet()) {
+                if (map.containsKey(key) && !shape.get(key).isValid(map.get(key))) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
     public void sizeof(int size) {
         this.mapSize = size;
+    }
+
+    public void shape(Map<String, BaseSchema> map) {
+        this.shape = map;
     }
 }
