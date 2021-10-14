b2621324f2a1327fc52e6201073fb2fcda91f603
Sergey Afonasyev <Afonasiew.serzh@yandex.ru>
2021-10-14 20:53:41 +0300

    Remove magic numbers

--- src/test/java/hexlet/code/SchemasTest.java
+++ src/test/java/hexlet/code/SchemasTest.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
@@ -24,7 +25,9 @@ public class SchemasTest {
         Assertions.assertFalse(schema.isValid(null));
         Assertions.assertFalse(schema.isValid(""));
 
-        Assertions.assertFalse(schema.minLength(6).isValid("Hello"));
+        final int minLength = 6;
+
+        Assertions.assertFalse(schema.minLength(minLength).isValid("Hello"));
         Assertions.assertTrue(schema.isValid("Hello World!"));
 
         Assertions.assertTrue(schema.contains("what").isValid("what does the fox say"));
@@ -39,31 +42,36 @@ public class SchemasTest {
 
         schema.required();
 
+        final int six = 6;
+        final int ten = 10;
+        final int five = 5;
+
         Assertions.assertFalse(schema.isValid(null));
-        Assertions.assertTrue(schema.isValid(6));
+        Assertions.assertTrue(schema.isValid(six));
         Assertions.assertFalse(schema.isValid("5"));
 
-        Assertions.assertTrue(schema.positive().isValid(10));
-        Assertions.assertFalse(schema.isValid(-6));
+        Assertions.assertTrue(schema.positive().isValid(ten));
+        Assertions.assertFalse(schema.isValid(-six));
 
-        schema.range(5, 10);
+        schema.range(five, ten);
 
-        Assertions.assertTrue(schema.isValid(10));
-        Assertions.assertTrue(schema.isValid(5));
-        Assertions.assertFalse(schema.isValid(11));
-        Assertions.assertFalse(schema.isValid(4));
+        Assertions.assertTrue(schema.isValid(ten));
+        Assertions.assertTrue(schema.isValid(five));
+        Assertions.assertFalse(schema.isValid(ten + 1));
+        Assertions.assertFalse(schema.isValid(five - 1));
     }
 
     @Test
     void mapSchemaTest() {
-        MapSchema schema = new Validator().map();
+        Validator v = new Validator();
+        MapSchema schema = v.map();
 
         Assertions.assertTrue(schema.isValid(null));
 
         schema.required();
 
         Assertions.assertFalse(schema.isValid(null));
-        Assertions.assertTrue(schema.isValid(new HashMap()));
+        Assertions.assertTrue(schema.isValid(new HashMap<>()));
 
         Map<String, String> data = new HashMap<>();
         data.put("key1", "value1");
@@ -75,5 +83,33 @@ public class SchemasTest {
         Assertions.assertFalse(schema.isValid(data));
         data.put("key2", "value2");
         Assertions.assertTrue(schema.isValid(data));
+
+        Map<String, BaseSchema> schemas = new HashMap<>();
+        schemas.put("name", v.string().required());
+        schemas.put("age", v.number().positive());
+        schema.shape(schemas);
+
+        final int hundred = 100;
+
+        Map<String, Object> human1 = new HashMap<>();
+        human1.put("name", "Kolya");
+        human1.put("age", hundred);
+        Assertions.assertTrue(schema.isValid(human1));
+
+        Map<String, Object> human2 = new HashMap<>();
+        human2.put("name", "Maya");
+        human2.put("age", null);
+        Assertions.assertTrue(schema.isValid(human2));
+
+
+        Map<String, Object> human3 = new HashMap<>();
+        human3.put("name", "");
+        human3.put("age", null);
+        Assertions.assertFalse(schema.isValid(human3));
+
+        Map<String, Object> human4 = new HashMap<>();
+        human4.put("name", "Valya");
+        human4.put("age", -hundred);
+        Assertions.assertFalse(schema.isValid(human4));
     }
 }
